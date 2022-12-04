bb2a0b9b474b46cb282d22c5dc33bf459766f67d
terry255180 <119744133+terry255180@users.noreply.github.com>
2022-12-04 21:48:31 +0800

    Add files via upload

---
+++ routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :keyword_mappings
+  resources :push_messages, only: [:new, :create]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/kamigo/eat', to: 'kamigo#eat'
+  get '/kamigo/request_headers', to: 'kamigo#request_headers'
+  get '/kamigo/request_body', to: 'kamigo#request_body'
+  get '/kamigo/response_headers', to: 'kamigo#response_headers'
+  get '/kamigo/response_body', to: 'kamigo#show_response_body'
+  get '/kamigo/sent_request', to: 'kamigo#sent_request'
+  post '/kamigo/webhook', to: 'kamigo#webhook'
+end
